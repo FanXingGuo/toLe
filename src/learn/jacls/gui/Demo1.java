634dcd1e90ae06d2f9270c432bbf40f6864ef0dd
FanXingGuo <study1997.fxg@hotmail.com>
2018-09-08 15:59:16 +0800

    增加获取路径显示目录 增加错误提示对话框 和回车转到

--- src/learn/jacls/gui/Demo1.java
+++ src/learn/jacls/gui/Demo1.java
@@ -15,6 +15,9 @@ public class Demo1 {
     private Button button;
     private TextField textField;
     private TextArea textArea;
+    private Dialog dialog;
+    private Button okbtn;
+    private Label label;
 
     Demo1(){
         init();
@@ -24,6 +27,10 @@ public class Demo1 {
         button=new Button("转到");
         textField =new TextField(20);
         textArea =new TextArea(10,30);
+        dialog=new Dialog(frame,"提示信息",true);
+        label=new Label();
+        okbtn=new Button("确定");
+
 
 
         frame.setBounds(100,100,300,400);
@@ -31,6 +38,11 @@ public class Demo1 {
         frame.add(textField);
         frame.add(button);
         frame.add(textArea);
+
+        dialog.setBounds(30,30,200,100);
+        dialog.setLayout(new FlowLayout());
+        dialog.add(label);
+        dialog.add(okbtn);
 
         myEvn();
 
@@ -46,17 +58,7 @@ public class Demo1 {
                 System.exit(0);
             }
         });
-        //活动监听
-//        button.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                System.out.printf("按钮 退出");
-//                System.exit(0);
-//            }
-//        });
 
-
-        //鼠标事件
         button.addMouseListener(new MouseAdapter() {
             int count=0;
             @Override
@@ -66,50 +68,56 @@ public class Demo1 {
             }
         });
 
-        //鼠标双击
-//        button.addMouseListener(new MouseAdapter() {
-//            @Override
-//            public void mouseClicked(MouseEvent e) {
-//                if(e.getClickCount()==2){
-//                    System.out.println("鼠标双击!");
-//                }
-//            }
-//        });
 
-        //键盘事件
-//        textField.addKeyListener(new KeyAdapter() {
-//            @Override
-//            public void keyPressed(KeyEvent e) {
-//                System.out.println("getKeyChar():"+e.getKeyChar()+",code:"+e.getKeyCode());
-//            }
-//        });
-
-//        textField.addKeyListener(new KeyAdapter() {
-//            @Override
-//            public void keyPressed(KeyEvent e) {
-////                if(e.isControlDown()&&e.getKeyCode()==KeyEvent.VK_ENTER){
-////                    System.out.println("ctrl+enter 按下");
-////                }
-//                int code=e.getKeyCode();
-//                if(!(code>=KeyEvent.VK_0&&code<=KeyEvent.VK_9)){
-//                    System.out.println("输入不合法");
-//                    e.consume();
-//                }
-//            }
-//        });
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String st=textField.getText();
-                File file=new File(st);
-                if(file.exists()&&file.isDirectory()){
-                    String [] names=file.list();
-                    for (String name:names){
-                        textArea.append(name+"\r\n");
-                    }
+                showList();
+            }
+        });
+
+        okbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dialog.setVisible(false);
+            }
+        });
+
+        dialog.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dialog.setVisible(false);
+            }
+        });
+
+        textField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode()==KeyEvent.VK_ENTER){
+                    showList();
                 }
             }
         });
+    }
+
+    private void showDio(){
+        String st=textField.getText();
+        label.setText("您输入的地址:"+st+",不合法请重输");
+        dialog.setVisible(true);
+    }
+
+    private void showList(){
+        String st=textField.getText();
+        File file=new File(st);
+        if(file.exists()&&file.isDirectory()){
+            String [] names=file.list();
+            for (String name:names){
+                textArea.append(name+"\r\n");
+            }
+        }
+        else {
+            showDio();
+        }
     }
 
 
